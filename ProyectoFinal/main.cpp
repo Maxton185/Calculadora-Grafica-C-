58feba33c10d89ed8eb7920d3e51b8846560da84
Abonalde77 <abonalde@usm.cl>
2026-07-03 11:38:40 -0400

    Delete ProyectoFinal directory

--- ProyectoFinal/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "WindowGraficadora.h"
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    WindowGraficadora ventanaPrincipal;
-    ventanaPrincipal.show();
-    return app.exec();
-}
