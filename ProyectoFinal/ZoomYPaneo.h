37d707fcd4affd6193de35667fb3d0a42832aac2
Abonalde77 <abonalde@usm.cl>
2026-07-03 11:17:39 -0400

    Update ZoomYPaneo.h

--- ProyectoFinal/ZoomYPaneo.h
+++ ProyectoFinal/ZoomYPaneo.h
@@ -1,5 +1,5 @@
-#ifndef CUSTOMCHARTVIEW_H
-#define CUSTOMCHARTVIEW_H
+#ifndef ZOOMYPANEO_H
+#define ZOOMYPANEO_H
 
 #include <QChartView>
 #include <QLabel>
@@ -12,84 +12,20 @@ using namespace QtCharts;
 class ZoomYPaneo : public QChartView {
     Q_OBJECT 
 private:
-    bool m_isPanning = false;   
-    QPoint m_lastMousePos;      
+    bool m_isPanning = false;
+    QPoint m_lastMousePos;
     QLabel* m_labelCoordenadas = nullptr; 
 
 protected:
-    void resizeEvent(QResizeEvent *event) override;
-    void wheelEvent(QWheelEvent *event) override;
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *event) override;
+    void resizeEvent(QResizeEvent event) override;
+    void wheelEvent(QWheelEventevent) override;
+    void mousePressEvent(QMouseEvent event) override;
+    void mouseMoveEvent(QMouseEventevent) override;
+    void mouseReleaseEvent(QMouseEvent event) override;
 
 public:
-    explicit ZoomYPaneo(QChart *chart, QWidget *parent = nullptr);
-    void setLabelCoordenadas(QLabel* label);
+    explicit ZoomYPaneo(QChartchart, QWidget parent = nullptr);
+    void setLabelCoordenadas(QLabel label);
 };
 
-#endif // CUSTOMCHARTVIEW_H
-CustomChartView.cpp
-
-C++
-#include "CustomChartView.h"
-
-CustomChartView::CustomChartView(QChart *chart, QWidget *parent) : QChartView(chart, parent) {
-    setRubberBand(QChartView::NoRubberBand); 
-    setRenderHint(QPainter::Antialiasing); 
-    setMouseTracking(true); 
-}
-
-void CustomChartView::setLabelCoordenadas(QLabel* label) {
-    m_labelCoordenadas = label;
-}
-
-void CustomChartView::resizeEvent(QResizeEvent *event) {
-    QChartView::resizeEvent(event);
-    this->viewport()->update();
-}
-
-void CustomChartView::wheelEvent(QWheelEvent *event) {
-    qreal factor = (event->angleDelta().y() > 0) ? 1.1 : 0.9;
-    chart()->zoom(factor);
-    event->accept(); 
-}
-
-void CustomChartView::mousePressEvent(QMouseEvent *event) {
-    if (event->button() == Qt::RightButton) {
-        m_isPanning = true;
-        m_lastMousePos = event->pos();
-        setCursor(Qt::ClosedHandCursor); 
-        event->accept();
-    } else {
-        QChartView::mousePressEvent(event);
-    }
-}
-
-void CustomChartView::mouseMoveEvent(QMouseEvent *event) {
-    if (m_labelCoordenadas) {
-        QPointF mathPoint = chart()->mapToValue(event->pos());
-        m_labelCoordenadas->setText(QString("X: %1\nY: %2")
-                                    .arg(mathPoint.x(), 0, 'f', 3)
-                                    .arg(mathPoint.y(), 0, 'f', 3));
-    }
-
-    if (m_isPanning) {
-        QPoint delta = event->pos() - m_lastMousePos;
-        m_lastMousePos = event->pos();
-        chart()->scroll(-delta.x(), delta.y());
-        event->accept();
-    } else {
-        QChartView::mouseMoveEvent(event);
-    }
-}
-
-void CustomChartView::mouseReleaseEvent(QMouseEvent *event) {
-    if (event->button() == Qt::RightButton) {
-        m_isPanning = false;
-        setCursor(Qt::ArrowCursor); 
-        event->accept();
-    } else {
-        QChartView::mouseReleaseEvent(event);
-    }
-}
+#endif // ZOOMYPANEO_H
